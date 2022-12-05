e7ef3521e841ffcedb1deed288875e46f727f3ee
flash-of-genius <amhdaar@gmail.com>
2022-12-05 11:45:56 +0100

    bottom nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,21 @@ function Header() {
       </div>
 
       {/* Bottom nav */}
-      <div></div>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden md:inline">Electronics</p>
+        <p className="link hidden md:inline">Food & Grocery</p>
+        <p className="link hidden md:inline">Prime</p>
+        <p className="link hidden md:inline">Buy Again</p>
+        <p className="link hidden md:inline">Shopper Toolkit</p>
+        <p className="link hidden md:inline">Health & Personal Care</p>
+      </div>
     </header>
   );
 }
